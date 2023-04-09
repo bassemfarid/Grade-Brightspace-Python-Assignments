4609cd29f411870188d9fa32b22c7693e217d990
bassemfarid <71142299+Mr-Farid@users.noreply.github.com>
2023-04-09 16:22:46 -0400

    added several flake8 plugins

--- lint_files.py
+++ lint_files.py
@@ -1,3 +1,5 @@
+"""Lint all student files and write it to a text file."""
+
 import os
 import subprocess
 
@@ -7,47 +9,47 @@ ASSIGNMENTS_DIR = 'assignments'
 # Linting options for flake8
 # https://flake8.pycqa.org/en/latest/user/options.html
 FLAKE8_OPTIONS = [
-    '--max-line-length=79',  # default is 79
-    '--ignore=E123,E126,E133,E226,W503',
+    '--max-line-length=79',  # default is 79, PEP 8
+    '--ignore=E123,E126,E133,E226,W503,D205,D400',
     '--disable-noqa',  # In case NOQA is used for silencing
-    '--max-doc-length=72',  # PEP suggestion
+    '--max-doc-length=72',  # PEP 8 suggestion
     '--statistics',
-    # '--show-source',
 ]
 
 
-def lint_student_file(student_files):
-    """Lint all of the student's files using flake8"""
+def group_files():
+    """Rename and group student files."""
+    student_files = {}
+    for filename in os.listdir(ASSIGNMENTS_DIR):
+        if filename.endswith('.py'):
+            if '-' in filename:
+                dash_sep_list = filename.split('-')
+                student_name = dash_sep_list[2][1:].replace(' ', '_').lower()
+                new_filename = student_name + '_' + dash_sep_list[4][1:]
+                student_file = os.path.join(ASSIGNMENTS_DIR, filename)
+                new_student_file = os.path.join(ASSIGNMENTS_DIR, new_filename)
+                os.rename(student_file, new_student_file)
+                student_file = new_student_file
+            else:
+                student_file = os.path.join(ASSIGNMENTS_DIR, filename)
+                student_name = filename[:filename.rindex('_')]
+            if student_name not in student_files:
+                student_files[student_name] = []
+            student_files[student_name].append(student_file)
+    return student_files
+
+
+def lint_student_files(student_files):
+    """Lint all of the student's files using flake8."""
     args = ['flake8'] + FLAKE8_OPTIONS + student_files
     result = subprocess.run(args, capture_output=True, text=True)
     return result.stdout
 
 
-def group_files():
-    """Rename and group student files"""
-    student_files = {}
-    for filename in os.listdir(ASSIGNMENTS_DIR):
-        if filename.endswith('.py'):
-            dash_sep_list = filename.split('-')
-            student_name = dash_sep_list[2][1:].replace(' ', '_').lower() + '_'
-            if student_name not in student_files:
-                student_files[student_name] = []
-            new_filename = student_name + dash_sep_list[4][1:]
-            new_filename = new_filename
-            student_file = os.path.join(ASSIGNMENTS_DIR, filename)
-            new_student_file = os.path.join(ASSIGNMENTS_DIR, new_filename)
-            os.rename(student_file, new_student_file)
-            student_files[student_name].append(new_student_file)
-    return student_files
-
-
-def lint_all_student_files(student_files):
-    """
-    Lint all student files in the students directory and write the
-    results to text files
-    """
+def lint_all_students(student_files):
+    """Lint all student files."""
     for student in student_files:
-        lint_results = lint_student_file(student_files[student])
+        lint_results = lint_student_files(student_files[student])
         # Write lint results to text file
         output_filename = student + '_lint_results.txt'
         output_file = os.path.join(ASSIGNMENTS_DIR, output_filename)
@@ -56,11 +58,12 @@ def lint_all_student_files(student_files):
 
 
 def main():
+    """Run the main script."""
     if not os.path.exists(ASSIGNMENTS_DIR):
         print(f"Error: {ASSIGNMENTS_DIR} directory does not exist")
         return
     student_files = group_files()
-    lint_all_student_files(student_files)
+    lint_all_students(student_files)
 
 
 if __name__ == '__main__':
